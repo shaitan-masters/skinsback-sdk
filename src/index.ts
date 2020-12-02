57831bf4a77130e098af3bd34010d4f2d9023f0c
lobasya <naydyonovdanil@gmail.com>
2020-12-02 11:33:24 +0200

    Remove Api instance

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,3 @@ import WS from "./WebSockets";
 export * from './types';
 export const API = Api;
 export const WebSockets = WS;
-
-const api = new API({
-    shop_id: '2116',
-    secret_key: 'ZQflT12V2qK2UmGGHjr0',
-    logsPath: 'logs',
-    enableLogs: true,
-    excludeMethods: ['status'],
-    amountOfLastDaysOfSavingLogs: 3
-})
-
-api.getBalance().catch(e => {console.log(e)})
