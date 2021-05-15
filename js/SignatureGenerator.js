ea57ec7839937ba076e70155d9dbad42b9c63cec
Sergey Malyutkin <webmaster@skin.club>
2021-05-16 00:05:49 +0300

    Package & code update

--- js/SignatureGenerator.js
+++ js/SignatureGenerator.js
@@ -4,17 +4,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SignatureWebSocketsBuilder = exports.SignatureApiBuilder = void 0;
-const crypto_1 = require("crypto");
 const md5_1 = __importDefault(require("md5"));
+const crypto_1 = require("crypto");
 const SignatureApiBuilder = (params, secret_key) => {
-    let paramsString = Object.entries(params)
-        .sort()
-        .reduce((paramsAsString, [key, value]) => {
-        if (key === 'sign' || typeof key == 'object')
-            return paramsAsString;
-        return paramsAsString + key + ':' + value + ';';
-    }, '');
-    return crypto_1.createHmac('sha1', secret_key).update(paramsString).digest('hex');
+    let paramsString = "";
+    for (const [key, value] of Object.entries(params).sort()) {
+        if (key === "sign") {
+            continue;
+        }
+        if (typeof value === "object") {
+            continue;
+        }
+        paramsString += key + ":" + value + ";";
+    }
+    return crypto_1.createHmac("sha1", secret_key).update(paramsString).digest("hex");
 };
 exports.SignatureApiBuilder = SignatureApiBuilder;
 const SignatureWebSocketsBuilder = (shop_id, secret_key) => {
