1654449d48291d1b6d5a67e1928ea256b621c69c
VasiliyIsaichkin <vasiliy@isaichkin.ru>
2022-01-20 13:35:14 +0300

    Fix publication issue & clean repo

--- js/defaultConfig.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.WS_URL = exports.API_URL = void 0;
-exports.API_URL = 'https://skinsback.com/api.php';
-exports.WS_URL = 'ws://185.71.65.202:7777';
